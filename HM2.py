0168f0ad82feb202c3f00147ab83e32e3cc0feb9
ForzejNA <Dragoonsmash@vk.com>
2024-08-28 14:54:36 +0300

    Add files via upload

---
+++ HM2.py
@@ -0,0 +1,8 @@
+my_string = input('Введите значение: ' .upper())
+print(my_string)
+
+my_string = input('Введите значение: ' .lower())
+print(my_string)
+
+my_string = input('Введите значение: ' .replace(" ", "#"))
+print(my_string)
