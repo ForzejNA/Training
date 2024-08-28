530916d25154d28175aa80421eba7bf27eafcee5
ForzejNA <Dragoonsmash@vk.com>
2024-08-28 13:54:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+Homework = "12"
+hours = "1.5"
+where = "python"
+time = "0,125"
+print (Homework, hours, where, time)
+
+Homework = "12"
+print(Homework)
+hours = "1.5"
+print(hours)
+where = "python"
+print(where)
+time = "0,125"
+print(time)
