e46991fba36a317cccbbd7c6825b265961f4bcf9
ForzejNA <Dragoonsmash@vk.com>
2025-01-06 11:02:47 +0300

    Update main.py
    Like this'

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-Homework = "12"
-hours = "1.5"
+Homework = 12
+hours = 1.5
 where = "python"
-time = "0,125"
+time = 0,125
 print (Homework, hours, where, time)
-
-Homework = "12"
-print(Homework)
-hours = "1.5"
-print(hours)
-where = "python"
-print(where)
-time = "0,125"
-print(time)
