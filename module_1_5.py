741db4d447cdf2b4608a07a7176db6ca67518359
ForzejNA <Dragoonsmash@vk.com>
2025-01-13 00:55:25 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2, 'a', 'b')
+print(immutable_var) #Кортеж нельзя изменять потому что, это неизменяемый список
+
+Mutable_list = [1, 2, 'a' 'b']
+Mutable_list.append("Modified")
+print(Mutable_list)
