5d975b8a0cb4e9de33e00b8e35e5f5b91b41e8dc
ForzejNA <Dragoonsmash@vk.com>
2025-01-14 14:11:48 +0300

    Add files via upload

---
+++ For i In.py
@@ -0,0 +1,22 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) +1 ):
+        if num % i == 0:
+            return True
+        return False
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+prime = []
+non_prime = []
+
+for num in numbers:
+    if is_prime(num):
+        prime.append(num)
+    else:
+        non_prime.append(num)
+
+print("Простые числа:", prime)
+print("Не простые числа:", non_prime)
+
